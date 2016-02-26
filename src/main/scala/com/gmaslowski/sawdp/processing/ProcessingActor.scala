dab35eb7a2869dc935278eb2eee0f0b13039cd29
gmaslowski <gregmaslowski@gmail.com>
2016-02-26 21:43:01 +0100

    Implementing Processor and Scheduler.

--- src/main/scala/com/gmaslowski/sawdp/processing/ProcessingActor.scala
+++ src/main/scala/com/gmaslowski/sawdp/processing/ProcessingActor.scala
@@ -1,5 +1,30 @@
 package com.gmaslowski.sawdp.processing
 
-class ProcessingActor {
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import com.gmaslowski.sawdp.processing.ProcessingActor.{ProcessMessage, ProcessingStatus, RescheduledMessage}
 
+object ProcessingActor {
+
+  case class ProcessMessage(responseTo: ActorRef, content: String)
+  case class RescheduledMessage(responseTo: ActorRef, content: String)
+  case class ProcessingStatus(content: String)
+
+}
+
+class ProcessingActor extends Actor with ActorLogging {
+
+  import scala.concurrent.duration._
+  implicit val ec = context.dispatcher
+
+  override def receive = {
+    case ProcessMessage(actorToNofity, content) =>
+      context.system.scheduler.schedule(
+        1 seconds,
+        2 seconds,
+        self,
+        new RescheduledMessage(actorToNofity, content))
+
+    case RescheduledMessage(actorToNotify, content) =>
+      actorToNotify ! ProcessingStatus(content)
+  }
 }
--- src/main/scala/com/gmaslowski/sawdp/processing/SchedulingActor.scala
+++ src/main/scala/com/gmaslowski/sawdp/processing/SchedulingActor.scala
@@ -1,5 +1,22 @@
 package com.gmaslowski.sawdp.processing
 
-class SchedulingActor {
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import com.gmaslowski.sawdp.processing.ProcessingActor.ProcessMessage
+import com.gmaslowski.sawdp.processing.SchedulingActor.ScheduleProcessing
 
+object SchedulingActor {
+
+  def props(processingActor: ActorRef) = Props(classOf[SchedulingActor], processingActor)
+
+  case class ScheduleProcessing(content: String)
+
+}
+
+class SchedulingActor(processingActor: ActorRef) extends Actor with ActorLogging {
+
+  override def receive = {
+
+    case ScheduleProcessing(content) =>
+      processingActor ! new ProcessMessage(self, content)
+  }
 }
